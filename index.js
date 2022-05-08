6043a027629376777944e0a02daccad72a570960
Almas Khan <almas.rbm@gmail.com>
2022-05-09 01:07:07 +0600

    update a item quantity

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+    origin: true,
+    credential: true,
+}
+app.use(cors(corsConfig));
+app.options('*', cors(corsConfig));
 app.use(express.json());
 
 
@@ -31,6 +37,21 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const item = await itemCollection.findOne(query);
             res.send(item);
+        })
+
+        //Update a item(quantity)
+        app.put('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedItemQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedItemQuantity.quantity,
+                }
+            };
+            const result = await itemCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
         })
 
     }
