d16c68425fb36a9d081e5163fe4ffe3c83f1f0ad
Almas Khan <almas.rbm@gmail.com>
2022-05-08 17:52:43 +0600

    GET Single item for Inventory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -17,13 +17,22 @@ async function run() {
         await client.connect();
         const itemCollection = client.db('warehouse').collection('item');
 
-        //GET item
+        //GET all item
         app.get('/item', async (req, res) => {
             const query = {};
             const cursor = itemCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
         })
+
+        //GET single item
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        })
+
     }
     finally {
 
